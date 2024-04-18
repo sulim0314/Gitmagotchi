9bc93e536646b1054c0a4d4db0c4b235c7a95b9e
송윤재 <1218song@naver.com>
2024-04-18 14:54:56 +0900

    ✨ (Feat) backend initial project
    Former-commit-id: c3f406b19fc6955abd575007c483d2421ecef447

---
+++ back/gitmagotchi/src/test/java/com/ssafy/gitmagotchi/GitmagotchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.gitmagotchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmagotchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
