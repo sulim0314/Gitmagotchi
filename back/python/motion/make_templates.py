f1762ad803401beb7002b3a04e4bb8a2e3fed539
권송아 <gunsong2@naver.com>
2024-05-02 10:01:05 +0900

    ✨ [Feat/152] Make .gif to a sprite sheet
    Make .gif to a sprite sheet
    
    
    Former-commit-id: 19d56eba733435c7910a86ecd80af1704a417a96 [formerly 3ddd5141a37e2fd2f5214bb1564a371d22089fe6]
    Former-commit-id: 8f531e31875cda0a98e67de5bdd8f734c3e1ca09

---
+++ back/python/motion/make_templates.py
@@ -0,0 +1,22 @@
+import sys
+from gitmagotchi.motion.image_to_animation import image_to_animation
+from gitmagotchi.sprite_sheet.gif_to_sheet import gif_to_sheet
+import os
+
+def start(char_anno_dir: str, motion_cfg_fn: str):
+    if char_anno_dir is None:
+        char_anno_dir = "out"
+    
+    if motion_cfg_fn is None:
+        motion_cfg_fn = "AnimatedDrawings/gitmagotchi/config/motion/dab.yaml"
+    
+    retarget_cfg_fn = "AnimatedDrawings/gitmagotchi/config/retarget/fair1_ppf.yaml"
+    
+    # 이미지 생성
+    image_to_animation(char_anno_dir, motion_cfg_fn, retarget_cfg_fn)
+    
+    # sprite sheet 생성
+    gif_to_sheet(char_anno_dir)
+
+if __name__ == "__main__":
+    start(None, None)
