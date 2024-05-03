1c5ec19a80cf8fa7bc551eff67390999a97ae411
heeyeon <kho200a@naver.com>
2024-05-03 15:18:41 +0900

    ✨ [Feat/dev-backend] 채팅 감ì정 분석 기능 구현
    Former-commit-id: f64c83a64f402c38a0ce8c3f57387d42ea402715

---
+++ back/java/HelloWorldFunction/src/main/java/chat/sentiment/handler/AnalyzeSentimentHandler.java
@@ -0,0 +1,21 @@
+package chat.sentiment.handler;
+
+import chat.sentiment.service.DetectSentimentService;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class AnalyzeSentimentHandler implements RequestHandler<Map<String, Object>, String> {
+
+    @Override
+    public String handleRequest(Map<String, Object> requestObject, Context context) {
+        String sourceText = (String) requestObject.get("source_text");
+        context.getLogger().log("Extracted text: " + sourceText);
+        DetectSentimentService detectSentimentService = new DetectSentimentService();
+        JSONObject jsonOb = detectSentimentService.detectSentiments(sourceText);
+        context.getLogger().log("JSON: " + jsonOb.optString("sentiment", "No sentiment found"));
+        return jsonOb.optString("sentiment", "No sentiment found");
+    }
+}
