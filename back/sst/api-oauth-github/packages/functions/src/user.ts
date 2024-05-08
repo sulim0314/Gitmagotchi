40a96b6cf7b28c84626c47e100bd8432288d1ac3
KohSulim <sulim0314@naver.com>
2024-05-08 17:21:16 +0900

    ⛏ [Fix/220] signin-signup 람다 수정
    Former-commit-id: 14ca06a10d54c89670f2a20d41c7bac7119356f0 [formerly a389f2d4155d1f93084b04518440f8fc1fec99a2]
    Former-commit-id: a61ad915e3543856aa92deb7e38b879623d4285e

--- back/sst/api-oauth-github/packages/functions/src/user.ts
+++ back/sst/api-oauth-github/packages/functions/src/user.ts
@@ -14,48 +14,26 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   ).json();
 
   // AWS SDK를 사용하여 SQS 클라이언트 초기화
-  const AWS = require("aws-sdk");
-  const sqs = new AWS.SQS();
+  // const AWS = require("aws-sdk");
+  // const sqs = new AWS.SQS();
 
-  const params = {
-    MessageBody: JSON.stringify({
-      userId: token.id,
-      githubUsername: token.login,
-      profileImg: token.avatar_url,
-      nickname: token.name,
-    }),
-    QueueUrl:
-      "https://sqs.ap-northeast-2.amazonaws.com/992382698264/SignInOrSignUp.fifo", // SQS 큐test URL
-    MessageGroupId: "AuthUser", // 메시지 그룹 ID
-  };
+  // const params = {
+  //   MessageBody: JSON.stringify({
+  //     userId: token.id,
+  //     githubUsername: token.login,
+  //     profileImg: token.avatar_url,
+  //     nickname: token.name,
+  //   }),
+  //   QueueUrl:
+  //     "https://sqs.ap-northeast-2.amazonaws.com/992382698264/SignInOrSignUp.fifo",
+  //   MessageGroupId: "AuthUser", // 메시지 그룹 ID
+  // };
 
   // SQS 큐에 메시지 추가
-  await sqs.sendMessage(params).promise();
+  // await sqs.sendMessage(params).promise();
 
   return {
     statusCode: 200, // HTTP 응답 코드 추가
     body: JSON.stringify({ status: "Message sent", sub: token.id, ...token }),
-  };
-
-  // await fetch(
-  //   // "https://e344okbxno7radfxh4buxxmbse0ylorj.lambda-url.ap-northeast-2.on.aws",
-  //   "https://nrxxpka7lay3zbuw36hb2yzypi0iierp.lambda-url.ap-northeast-2.on.aws",
-  //   {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       id: token.id,
-  //       login: token.login,
-  //       avatar_url: token.avatar_url,
-  //       name: token.name,
-  //     }),
-  //   }
-  // );
-
-  return {
-    sub: token.id,
-    ...token,
   };
 };
